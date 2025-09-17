34386434afb760c754e871569a2f5605a1b781dc
Aaron Dolley <dolleyaaron@gmail.com>
2025-09-17 19:41:03 +0200

    database connected to app

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+require('dotenv').config({debug: true});
 const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const pool = require('./db');
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-require('dotenv').config();
+
+//testing the connection to the database
+app.get('/db-test', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json({
+          message:'Datasebase is connected successfully',
+          time: result.rows[0].now
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+  });  
 
 //Routes
 app.use('/', cors(), async (req, res) => {
